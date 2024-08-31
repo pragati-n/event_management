48a24b8a69ec0f1c67678fd77165f501a8ac094b
Pragati Naik <pragati.naik@kilowott.com>
2024-08-31 14:32:54 +0530

    Initial commit - project skeleton

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # event_management
 Advanced Event Management System
+
+The Project is handled using core php.
+The project is using MVC structure.
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define('ROOT' , __DIR__.'/') ;
+define('API_ROOT' , __DIR__.'/api/') ;
+define('API_PATH' , __DIR__.'/api/controllers') ;
+define('API_DOC_ROOT' , $_SERVER['SERVER_NAME'].'/events-management/index.php/api/') ;
